43fa46c916e11e3c3c27e3489f264268e45e0e81
Austin Lehman <lehmanaustin0@gmail.com>
2023-02-11 14:46:28 -0600

    added web dependency and test endpoint

--- src/main/java/com/groupfour/testcoveragetool/TestCoverageToolApplication.java
+++ src/main/java/com/groupfour/testcoveragetool/TestCoverageToolApplication.java
@@ -1,11 +1,21 @@
 package com.groupfour.testcoveragetool;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableAutoConfiguration
 @SpringBootApplication
 public class TestCoverageToolApplication {
 
+	@RequestMapping("/")
+	String ping() {
+		return "pong";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TestCoverageToolApplication.class, args);
 	}
