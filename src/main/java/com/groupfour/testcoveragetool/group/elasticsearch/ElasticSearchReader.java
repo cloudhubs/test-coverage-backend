8396a42cc3038cf1b705b6b4fda3cf5a285cd5df
Austin_Lehman1 <Austin_Lehman1@baylor.edu>
2023-03-22 13:30:59 -0500

    Wrote parser of logs

--- src/main/java/com/groupfour/testcoveragetool/group/elasticsearch/ElasticSearchReader.java
+++ src/main/java/com/groupfour/testcoveragetool/group/elasticsearch/ElasticSearchReader.java
@@ -1,49 +1,74 @@
 package com.groupfour.testcoveragetool.group.elasticsearch;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.*;
 
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.elasticsearch._types.ElasticsearchException;
-import co.elastic.clients.elasticsearch.core.SearchResponse;
-import co.elastic.clients.elasticsearch.core.search.Hit;
-import co.elastic.clients.json.jackson.JacksonJsonpMapper;
-import co.elastic.clients.transport.ElasticsearchTransport;
-import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 
 public class ElasticSearchReader {
 
+	public static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 
-	// Create the low-level client
-	public static RestClient restClient = RestClient.builder(
-	    new HttpHost("localhost", 9200)).build();
+	public static void getLogsInTimeRange(Date startTime, Date stopTime) throws IOException, ParseException {
+		init(); //refactor eventually
 
-	// Create the transport with a Jackson mapper
-	public static ElasticsearchTransport transport = new RestClientTransport(
-	    restClient, new JacksonJsonpMapper());
+		Date start = dateFormat.parse("2023-03-22T18:21:00.000Z"); //start time to query
+		Date stop = dateFormat.parse("2023-03-22T18:59:59.999Z"); //stop time to query
 
-	// And create the API client
-	public static ElasticsearchClient client = new ElasticsearchClient(transport);
+		getLogs(start, stop);
+	}
 
-	public static void main(String[] args) throws ElasticsearchException, IOException {
-		String index = "";
-		
-		SearchResponse<String> search = client.search(s -> s
-		    .index(index)
-		    .query(q -> q
-		        .term(t -> t
-		            .field("")
-		            .value(v -> v.stringValue(""))
-		        )),
-		    String.class);
-		
-		for (Hit<String> hit: search.hits().hits()) {
-		    System.out.println(hit.source());
+	public static void init() {
+		dateFormat.setTimeZone(TimeZone.getTimeZone("US/Central")); //set up for central timezone
+	}
+
+
+	public static void getLogs(Date start, Date stop) throws IOException {
+		String startStr = dateFormat.format(start);
+		String stopStr = dateFormat.format(stop);
+
+		RestHighLevelClient rhlc = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200)));
+		SearchRequest request = new SearchRequest("*"); //match all indices
+		SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+
+		//the actual query lol
+		searchSourceBuilder.query(QueryBuilders.boolQuery()
+				.must(QueryBuilders.rangeQuery("@timestamp") //query based on timestamp
+						.gte(startStr) //get all greater than
+						.lte(stopStr)) //get all less than
+				.must(QueryBuilders.queryStringQuery("*GET* OR *POST* OR *PUT* OR *DELETE*").field("message")) //get all that contain GET, PUT, POST, etc.
+		);
+
+
+		searchSourceBuilder.size(10000);
+		request.source(searchSourceBuilder);
+
+
+		SearchResponse searchResponse = rhlc.search(request, RequestOptions.DEFAULT); //perform the request
+		SearchHits hits = searchResponse.getHits();
+
+
+
+		for(SearchHit hit:hits.getHits()) {
+			String source = hit.getSourceAsString();
+			System.out.println(source);
 		}
 
+		System.out.println("\nTotal hits: " + hits.getTotalHits() + '\n');
 
+
+		rhlc.close();
 	}
 
 }
