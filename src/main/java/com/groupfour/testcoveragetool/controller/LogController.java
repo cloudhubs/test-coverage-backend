979edfd01dafbbf5365d8a7d2c980abe83a442ca
donovanorellana <Donovan_Orellana1@baylor.edu>
2023-04-03 14:08:19 -0500

    Update LogController.java

--- src/main/java/com/groupfour/testcoveragetool/controller/LogController.java
+++ src/main/java/com/groupfour/testcoveragetool/controller/LogController.java
@@ -2,6 +2,7 @@ package com.groupfour.testcoveragetool.controller;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -21,9 +22,16 @@ public class LogController {
 	private ElasticSearchReader logReader = new ElasticSearchReader();
 	private String field;
 	private List<String> regexList;
-	
+	private boolean fieldLock = true;
+	private boolean regexListLock = true;
+
 	@GetMapping("/endpoints")
 	public List<String> getAllEndpoints() {
+		while (this.fieldLock || this.regexListLock);
+
+		this.fieldLock = true;
+		this.regexListLock = true;
+
 		List<String> endpointsTested = null;
 		
 		/* Maybe we want to make a test class where we pass the test file, and just call a method to run it and return the enpoints??? */
@@ -41,12 +49,16 @@ public class LogController {
 	@ResponseBody
 	public void getField(@RequestParam String field) {
 		this.field = field;
+
+		this.fieldLock = false;
 	}
 
 	@PostMapping("/regexList")
 	@ResponseBody
 	public void getRegexList(@RequestParam List<String> regexList) {
-		this.regexList.addAll(regexList);
+		this.regexList = new ArrayList<>(regexList);
+
+		this.regexListLock = false;
 	}
 	
 	
